dce6b5c09647d499344300aa6a08d7811693cb25
Natalya Makarevich <mak.nathalie@gmail.com>
2021-07-18 16:08:59 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 4999;
+        int milePrice = 20;
+        //количество рублей для начисления 1 мили
+        int bonusMile = price / milePrice;
+        System.out.println(bonusMile);
+    }
+}
